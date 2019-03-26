dbcd7966cf3a028bd47117475037ff86dbde6322
Chuck Witt <witt.chuck@gmail.com>
2019-03-26 15:44:53 -0400

    adding python directory and ctypes wrapper for deft (stripped of functionality not yet public).

---
+++ python/deft.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import ctypes as ct
+import numpy as np
+
+lib = ct.cdll.LoadLibrary('/home/wcw/codes/deft/lib/libdeft.so')
+
+class deft(object):
+
+    def __init__(self, nx, ny, nz, vecx, vecy, vecz):
+
+        a1 = (ct.c_double * 3) (*vecx)
+        a2 = (ct.c_double * 3) (*vecy)
+        a3 = (ct.c_double * 3) (*vecz)
+
+        lib.deft_c.argtypes = [ct.c_int, ct.c_int, ct.c_int, \
+            ct.POINTER(ct.c_double), ct.POINTER(ct.c_double), ct.POINTER(ct.c_double)]
+        lib.deft_c.restype = ct.c_void_p
+
+        lib.equals_c.argtypes = [ct.c_void_p, ct.c_double]
+        lib.equals_c.restype = ct.c_void_p
+
+        lib.at_c.argtypes = [ct.c_void_p, ct.c_int, ct.c_int, ct.c_int]
+        lib.at_c.restype = ct.c_double
+
+        lib.integrate_c.argtypes = [ct.c_void_p]
+        lib.integrate_c.restype = ct.c_double
+
+        lib.interpolate_c.argtypes = [ct.c_size_t, ct.c_size_t, ct.c_size_t]
+        lib.interpolate_c.restype = ct.c_void_p
+
+        self.obj = lib.deft_c(nx, ny, nz, a1, a2, a3)
+
+    def equals(self, val):
+        lib.equals_c(self.obj, val)
+
+    def at(self, i, j, k):
+        return lib.at_c(self.obj, i, j, k)
+
+    def copyDataFrom(self, data):
+        return lib.copyDataFrom_c(self.obj, data.ctypes.data_as(ct.POINTER(ct.c_double)))
+
+    def integrate(self):
+        return lib.integrate_c(self.obj)
+
+    def interpolate(self, new_x, new_y, new_z):
+        return lib.interpolate_c(self.obj, new_x, new_y, new_z)
+
+
+def fourier_interpolate(grd, new_x, new_y, new_z, ax, ay, az):
+
+    grd_deft = deft(grd.shape[0], grd.shape[1], grd.shape[2], ax, ay, az)
+    grd_deft.copyDataFrom(grd)
+
+    new_x_c = ct.c_size_t(new_x); new_y_c = ct.c_size_t(new_y); new_z_c = ct.c_size_t(new_z)
+    arr_deft = deft(new_x, new_y, new_z, ax, ay, az)
+    arr_deft.obj = grd_deft.interpolate(new_x_c, new_y_c, new_z_c)
+    arr = np.require(np.zeros([new_x,new_y,new_z]), dtype='float64', requirements=['F_CONTIGUOUS', 'ALIGNED'])
+    for k in range(new_x):
+        for j in range(new_y):
+            for i in range(new_z):
+                arr[i,j,k] = arr_deft.at(i,j,k)
+    return arr
+    
