50a761fc3e5dbabfcd7c68370e82f5410d0d75c5
cw-tan <cw@LAPTOP-2GEGH1FF.localdomain>
2022-08-01 14:28:50 +0100

    fix structure_factor_spline bug

--- source/lattice_sum.cpp
+++ source/lattice_sum.cpp
@@ -144,12 +144,17 @@ Complex3D structure_factor_spline(
         r[2] = m[2][0]*v[0] + m[2][1]*v[1] + m[2][2]*v[2];
         return r;
     };
-    // compute fractional coordinates
+
     std::vector<std::array<double,3>> frac_coords(xyz_coords.size());
     for (size_t i=0; i<frac_coords.size(); ++i) {
         frac_coords[i] = m_dot_v(ainv, xyz_coords[i]);
-        //for (size_t j=0; j<3; ++j)
-        //    frac_coords[i][j] -= std::floor(frac_coords[i][j]);
+        for (size_t j=0; j<3; ++j) {
+            // compute fractional coordinates and make sure they lie in [0,1)
+            // the duplicated operation is intentional as a single operation fails to treat 
+            // small, negative values (e.g. -1e-20) that might be mapped to 1 instead of 0
+            frac_coords[i][j] -= std::floor(frac_coords[i][j]);
+            frac_coords[i][j] -= std::floor(frac_coords[i][j]);
+        }
     }
 
     int N0 = shape[0];
